c7991fd1d40890cbb8756099bad8dc3512af02f7
fritz-alano <fjerald.alano@gmail.com>
2022-10-20 10:07:34 -0600

    Initial user form setup using Formly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
+
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
 import { UsersComponent } from './users/users.component';
 import { AddComponent } from './users/add/add.component';
 import { EditComponent } from './users/edit/edit.component';
 import { FormComponent } from './users/shared/form/form.component';
-import { AppRoutingModule } from './app-routing.module'; 
 
 @NgModule({
   declarations: [
@@ -24,7 +29,10 @@ import { AppRoutingModule } from './app-routing.module';
     StoreModule.forRoot({}, {}),
     EffectsModule.forRoot([]),
     StoreRouterConnectingModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormlyModule.forRoot(),
+    FormlyBootstrapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/users/shared/form/form.component.ts
+++ src/app/users/shared/form/form.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormlyFieldConfig } from '@ngx-formly/core';
 
 @Component({
   selector: 'app-form',
@@ -6,10 +8,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+  form = new FormGroup({});
+  model = { firstname: null, lastname: null, email: 'email@gmail.com' };
+  fields: FormlyFieldConfig[] = [
+    {
+      key: 'firstname',
+      type: 'input',
+      props: {
+        label: 'First Name',
+        placeholder: 'Enter first name',
+        required: true,
+      }
+    },
+    {
+      key: 'lasatname',
+      type: 'input',
+      props: {
+        label: 'Laast Name',
+        placeholder: 'Enter last name',
+        required: true,
+      }
+    },
+    {
+      key: 'email',
+      type: 'input',
+      props: {
+        label: 'Email address',
+        placeholder: 'Enter email',
+        required: true,
+      }
+    }
+  ];
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+
+
+  onSubmit(model: any) {
+    console.log(model);
+  }
 }
